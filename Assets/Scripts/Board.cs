0e68ab0514bf93f06cd898ab864516edabd5bda9
Axel Nyström Svensson <axel.nystrom-svensson@hotmail.com>
2021-10-07 09:23:15 +0200

    Refactor of board backend logic
    It is now handled by a seperate class. Doing this makes it easier to search for hypothetical moves without actually acting on them.

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -7,24 +7,112 @@ public struct DiskStruct{
     public bool color;
 }
 
-public class Board
-{
+public class Board{
     private DiskStruct[,] board;
+    private int boardSize;
 
-    public Board(int size){
-        this.board = new DiskStruct[size,size];
+    private readonly Vector2Int[] directions = new Vector2Int[]{
+        new Vector2Int(0, 1),
+        new Vector2Int(1, 1),
+        new Vector2Int(1, 0),
+        new Vector2Int(1, -1),
+        new Vector2Int(0, -1),
+        new Vector2Int(-1, -1),
+        new Vector2Int(-1, 0),
+        new Vector2Int(-1, 1),
+    };
+
+    public Board(int size, DiskStruct[,] configuration = null){
+        if(configuration != null)
+            this.board = configuration;
+        else
+            this.board = new DiskStruct[size,size];
+        this.boardSize = size;
     }
 
-    public void PlaceDisk(Vector2Int position, bool color){
+    public bool TryPlaceDisk(Vector2Int position, bool color, bool force = false){
+        if(!IsWithinBoard(position))
+            return false;
+        if(this.board[position.x, position.y].isPlaced)
+            return false;
+        if(!force && !IsLegalMove(position, color))
+            return false;
+
         this.board[position.x, position.y].isPlaced = true;
         this.board[position.x, position.y].color = color;
+        return true;
     }
 
-    public bool FlipDisk(Vector2Int position){
+    public bool TryFlipDisk(Vector2Int position){
+        if(!IsWithinBoard(position))
+            return false;
         if(!this.board[position.x, position.y].isPlaced)
             return false;
 
         this.board[position.x, position.y].color = !this.board[position.x, position.y].color;
+        return true;
+    }
+
+    public void RemoveDisk(Vector2Int position){
+        if(!IsWithinBoard(position))
+            return;
+        this.board[position.x, position.y].isPlaced = false;
+    }
+
+    public List<Vector2Int> GetLegalMoves(bool color){
+        List<Vector2Int> legalMoves = new List<Vector2Int>(); 
+        for (int y = 0; y < boardSize; y++){
+            for (int x = 0; x < boardSize; x++){
+                if(board[x,y].isPlaced)  //cell already contains disk.
+                    continue;
+
+                Vector2Int currentPos = new Vector2Int(x,y);
+                if(IsLegalMove(currentPos, color))
+                    legalMoves.Add(currentPos);
+            }
+        }
+        return legalMoves;
+    }
+
+    public bool IsLegalMove(Vector2Int position, bool color){
+        for (int i = 0; i < directions.Length; i++){
+            Vector2Int previousPos = position - directions[i];
+            if(EvaluateDirection(position, previousPos, color, 0)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private bool EvaluateDirection(Vector2Int currentPos, Vector2Int previousPos, bool originalColor, int depth){
+        Vector2Int nextPos = currentPos + (currentPos - previousPos); //new Vector2Int(0, -1);
+        if(!IsWithinBoard(nextPos)){
+            // Debug.Log($"{nextPos} is not within board. Current: {currentPos}, Previous: {previousPos}, Next: {nextPos}");
+            return false;
+        }
+        
+        if(!board[nextPos.x, nextPos.y].isPlaced){
+            // Debug.Log("Reached a dead end.");
+            return false;
+        }
+
+        if(board[nextPos.x, nextPos.y].color == originalColor && depth >= 1){
+            // Debug.Log("Color was same and depth was more than or equal 1!");
+            return true;
+        }
+
+        if(board[nextPos.x, nextPos.y].color == originalColor && depth == 0){
+            // Debug.Log("Color was same and depth was more 0");
+            return false;
+        }
+
+        // Debug.Log("Going down");
+        return EvaluateDirection(nextPos, currentPos, originalColor, depth + 1);
+    }
+
+    private bool IsWithinBoard(Vector2Int position){
+        if(position.x >= boardSize || position.x < 0 || position.y >= boardSize || position.y < 0)
+            return false;
         return true;
     }
 }
--- Assets/Scripts/Gameboard.cs
+++ Assets/Scripts/Gameboard.cs
@@ -21,18 +21,12 @@ public class Gameboard : MonoBehaviour
     [SerializeField] private GameObject placementIndicator;
 
     private Camera mainCamera;
-    private Disk[,] board;
+    private Disk[,] disks;
+    private Board board;
 
-    private readonly Vector2Int[] directions = new Vector2Int[]{
-        new Vector2Int(0, 1),
-        new Vector2Int(1, 1),
-        new Vector2Int(1, 0),
-        new Vector2Int(1, -1),
-        new Vector2Int(0, -1),
-        new Vector2Int(-1, -1),
-        new Vector2Int(-1, 0),
-        new Vector2Int(-1, 1),
-    };
+    private bool currentPlayer = false;
+    private readonly bool playerColor = false;
+    private readonly bool opponentColor = true;
 
     [SerializeField] private DiscPosition[] initialPositions;
 
@@ -44,8 +38,8 @@ public class Gameboard : MonoBehaviour
 
         boardSize = settings.GetBoardSize();
         cellSize = settings.GetCellSize();
-
-        board = new Disk[boardSize,boardSize];
+        disks = new Disk[boardSize,boardSize];
+        board = new Board(boardSize);
         mainCamera = Camera.main;
 
         StartCoroutine(PlaceInitialDisks());
@@ -56,21 +50,38 @@ public class Gameboard : MonoBehaviour
 
         Vector2Int index;
         //Place black if right mouse button, otherwise place white. If disk is already in that spot, flip it.
-        if(Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(1)){
+        // if(Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(1)){
+        //     if(GetBoardIndexFromMousePosition(out index)){
+        //         if(!TryPlaceDisk(index, Input.GetMouseButtonDown(1))){
+        //             TryFlipDisk(index);
+        //         }
+        //     }
+        // }
+
+        if(Input.GetMouseButtonDown(0) && currentPlayer == playerColor){
             if(GetBoardIndexFromMousePosition(out index)){
-                if(!TryAddNewDisk(index, Input.GetMouseButtonDown(1))){
-                    if(IsWithinBoard(index))
-                    board[index.x, index.y].Flip();
+                if(TryPlaceDisk(index, playerColor)){
+                    NextPlayer();
                 }
             }
         }
 
-        if(Input.GetMouseButtonDown(2)){
+        if(Input.GetMouseButtonDown(2))
             if(GetBoardIndexFromMousePosition(out index))
-                if(IsWithinBoard(index) && board[index.x, index.y] != null){
-                    Destroy(board[index.x, index.y].gameObject);
-                    board[index.x, index.y] = null; 
-                }
+                if(IsWithinBoard(index) && disks[index.x, index.y] != null)
+                    RemoveDisk(index);
+    }
+
+    private void NextPlayer(){
+        currentPlayer = !currentPlayer;
+        if(currentPlayer == playerColor){
+
+        }
+
+        if(currentPlayer == opponentColor){
+            List<Vector2Int> legalMoves = board.GetLegalMoves(currentPlayer);
+            TryPlaceDisk(legalMoves[Random.Range(0, legalMoves.Count)], currentPlayer);
+            NextPlayer();
         }
     }
 
@@ -84,54 +95,6 @@ public class Gameboard : MonoBehaviour
             }
             else
                 placementIndicator.SetActive(false);
-    }
-    
-
-    private List<Vector2Int> GetLegalMoves(bool color){
-        List<Vector2Int> legalMoves = new List<Vector2Int>(); 
-        for (int y = 0; y < boardSize; y++){
-            for (int x = 0; x < boardSize; x++){
-                if(board[x,y] != null)  //cell already contains disk.
-                    continue;
-
-                Vector2Int currentPos = new Vector2Int(x,y);
-                for (int i = 0; i < directions.Length; i++){
-                    Vector2Int previousPos = currentPos - directions[i];
-                    if(EvaluateDirection(currentPos, previousPos, color, 0)){
-                        legalMoves.Add(currentPos);
-                        break;  //this is already a valid move, no need to check the other directions.
-                    }
-                }
-            }
-        }
-        return legalMoves;
-    }
-
-
-    private bool EvaluateDirection(Vector2Int currentPos, Vector2Int previousPos, bool originalColor, int depth){
-        Vector2Int nextPos = currentPos + (currentPos - previousPos); //new Vector2Int(0, -1);
-        if(!IsWithinBoard(nextPos)){
-            Debug.Log($"{nextPos} is not within board. Current: {currentPos}, Previous: {previousPos}, Next: {nextPos}");
-            return false;
-        }
-        
-        if(board[nextPos.x, nextPos.y] == null){
-            Debug.Log("Reached a dead end.");
-            return false;
-        }
-
-        if(board[nextPos.x, nextPos.y].GetColor() == originalColor && depth >= 1){
-            Debug.Log("Color was same and depth was more than or equal 1!");
-            return true;
-        }
-
-        if(board[nextPos.x, nextPos.y].GetColor() == originalColor && depth == 0){
-            Debug.Log("Color was same and depth was more 0");
-            return false;
-        }
-
-        Debug.Log("Going down");
-        return EvaluateDirection(nextPos, currentPos, originalColor, depth + 1);
     }
 
     /// <summary>
@@ -167,11 +130,16 @@ public class Gameboard : MonoBehaviour
     private IEnumerator PlaceInitialDisks(){
         // yield return new WaitForSeconds(0.5f);
         for (int i = 0; i < initialPositions.Length; i++){
-            TryAddNewDisk(initialPositions[i].position, initialPositions[i].color);
+            TryPlaceDisk(initialPositions[i].position, initialPositions[i].color, true);
             yield return new WaitForSeconds(0.01f);
         }
     }
 
+    /// <summary>
+    /// Get the board coordinates based on the onscreen mouse position.
+    /// </summary>
+    /// <param name="position"></param>
+    /// <returns>True if mouse was over a cell on the board.</returns>
     private bool GetBoardIndexFromMousePosition(out Vector2Int position){
         Ray ray = mainCamera.ViewportPointToRay(mainCamera.ScreenToViewportPoint(Input.mousePosition));
         Plane boardPlane = new Plane(transform.up, transform.position);
@@ -191,35 +159,38 @@ public class Gameboard : MonoBehaviour
     /// </summary>
     /// <param name="position">The gameboard coordinates on which to place the new disk.</param>
     /// <param name="color">What color the new disk should be. True = black, False = white</param>
+    /// <param name="force">Should this disk be placed regardless of the rules?</param>
     /// <returns>True if the new disk was successfully placed.</returns>
-    private bool TryAddNewDisk(Vector2Int position, bool color){
-        if(!IsWithinBoard(position))
+    private bool TryPlaceDisk(Vector2Int position, bool color, bool force = false){
+        if(!board.TryPlaceDisk(position, color, force))
             return false;
-        if(board[position.x, position.y] != null)
-            return false;
-    
+
         Vector3 spawnPoint = color ? blackSpawnPoint.position : whiteSpawnPoint.position;
         Disk newDisk = Instantiate(diskPrefab, spawnPoint, Quaternion.identity).GetComponent<Disk>();
         newDisk.Place(position, color);
-        board[position.x, position.y] = newDisk;
+        disks[position.x, position.y] = newDisk;
 
         return true;
+    }
+
+    private bool TryFlipDisk(Vector2Int position){
+        if(!board.TryFlipDisk(position))
+            return false;
+
+        disks[position.x, position.y].Flip();
+        return true;
+    }
+
+    private void RemoveDisk(Vector2Int position){
+        board.RemoveDisk(position);
+
+        Destroy(disks[position.x, position.y].gameObject);
+        disks[position.x, position.y] = null;
     }
 
     private bool IsWithinBoard(Vector2Int position){
         if(position.x >= boardSize || position.x < 0 || position.y >= boardSize || position.y < 0)
             return false;
         return true;
-    }
-
-    void OnDrawGizmos(){
-        if(board != null){
-            for (int y = 0; y < boardSize; y++){
-                for (int x = 0; x < boardSize; x++){
-                if(board[x,y] != null)
-                    Gizmos.DrawSphere(new Vector3(x * cellSize, 0, y * cellSize), 0.4f);
-                }
-            }
-        }
     }
 }
